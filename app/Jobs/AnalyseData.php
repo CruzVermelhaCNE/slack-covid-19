4c280ae77b3ab6ce4454a76ee4b352c5954eaf2b
Miguel Santos <github@miguelsantos.tech>
2020-02-28 09:22:28 +0000

    fix(AnalyseData): New contries and infected issues

--- app/Jobs/AnalyseData.php
+++ app/Jobs/AnalyseData.php
@@ -36,45 +36,60 @@ class AnalyseData implements ShouldQueue
         $region = Intel::select(["country","state"])->groupBy(["country","state"])->get();
         foreach ($region as $element) {
             $data = Intel::where('country', $element->country)->where('state', $element->state)->orderBy('created_at', 'desc')->limit(2)->get();
-            $infected = [
+            $infected = null;
+            $delta_confirmed = null;
+            $delta_infected = null;
+            $delta_recovered = null;
+            $delta_deaths = null;
+            if ($data->count == 2) {
+                $infected = [
                 0 => $data[0]->confirmed - $data[0]->recovered - $data[0]->deaths,
                 1 => $data[1]->confirmed - $data[1]->recovered - $data[1]->deaths
             ];
-            $delta_confirmed = $data[0]->confirmed - $data[1]->confirmed;
-            $delta_infected = $infected[0] - $infected[1];
-            $delta_recovered = $data[0]->recovered - $data[0]->recovered;
-            $delta_deaths = $data[0]->deaths - $data[0]->deaths;
+                $delta_confirmed = $data[0]->confirmed - $data[1]->confirmed;
+                $delta_infected = $infected[0] - $infected[1];
+                $delta_recovered = $data[0]->recovered - $data[1]->recovered;
+                $delta_deaths = $data[0]->deaths - $data[1]->deaths;
+            }
+            else {
+                $infected =  $data[0]->confirmed - $data[0]->recovered - $data[0]->deaths;
+                $delta_confirmed = $data[0]->confirmed;
+                $delta_infected = $infected[0];
+                $delta_recovered = $data[0]->recovered;
+                $delta_deaths = $data[0]->deaths;
+            }
 
-            if ($delta_confirmed != 0 || $delta_infected != 0 || $delta_recovered != 0 || $delta_deaths != 0) {
-                $confirmed_text = "";
-                $infected_text = "";
-                $recovered_text = "";
-                $deaths_text = "";
-                if ($delta_confirmed > 0) {
-                    $delta_confirmed = "+".$delta_confirmed;
-                } elseif ($delta_confirmed == 0) {
-                    $delta_confirmed = "=";
+                if ($delta_confirmed != 0 || $delta_infected != 0 || $delta_recovered != 0 || $delta_deaths != 0) {
+                    $confirmed_text = "";
+                    $infected_text = "";
+                    $recovered_text = "";
+                    $deaths_text = "";
+                    if ($delta_confirmed > 0) {
+                        $delta_confirmed = "+".$delta_confirmed;
+                    } elseif ($delta_confirmed == 0) {
+                        $delta_confirmed = "=";
+                    }
+                    $confirmed_text = $data[0]->confirmed . " (".$delta_confirmed.")";
+                    if ($delta_infected > 0) {
+                        $delta_infected = "+".$delta_infected;
+                    } elseif ($delta_infected == 0) {
+                        $delta_infected = "=";
+                    }
+                    $infected_text .= $infected[0] . " (".$delta_infected.")";
+                    if ($delta_recovered > 0) {
+                        $delta_recovered = "+".$delta_recovered;
+                    } elseif ($delta_recovered == 0) {
+                        $delta_recovered = "=";
+                    }
+                    $recovered_text = $data[0]->recovered . " (".$delta_recovered.")";
+                    if ($delta_deaths > 0) {
+                        $delta_deaths = "+".$delta_deaths;
+                    } elseif ($delta_recovered == 0) {
+                        $delta_recovered = "=";
+                    }
+                    $deaths_text = $data[0]->deaths . " (".$delta_deaths.")";
+                    $data[0]->notify(new InfectionUpdateToSlack($element->country, $element->state, $confirmed_text, $infected_text, $recovered_text, $deaths_text));
                 }
-                $confirmed_text = $data[0]->confirmed . " (".$delta_confirmed.")";
-                if ($delta_infected > 0) {
-                    $delta_infected = "+".$delta_infected;
-                } elseif ($delta_infected == 0) {
-                    $delta_infected = "=";
-                }
-                $infected_text .= $infected . " (".$delta_infected.")";
-                if ($delta_recovered > 0) {
-                    $delta_recovered = "+".$delta_recovered;
-                } elseif ($delta_recovered == 0) {
-                    $delta_recovered = "=";
-                }
-                $recovered_text = $data[0]->recovered . " (".$delta_recovered.")";
-                if ($delta_deaths > 0) {
-                    $delta_deaths = "+".$delta_deaths;
-                } elseif ($delta_recovered == 0) {
-                    $delta_recovered = "=";
-                }
-                $deaths_text = $data[0]->deaths . " (".$delta_deaths.")";
-                $data[0]->notify(new InfectionUpdateToSlack($element->country, $element->state, $confirmed_text, $infected_text, $recovered_text, $deaths_text));
             }
         }
     }
