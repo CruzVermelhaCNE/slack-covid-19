6b46bea47e30930b848a0b2fc9d001763b6f0d40
Miguel Santos <github@miguelsantos.tech>
2020-03-01 21:01:21 +0000

    chore(Jobs): Execute job every 10 minutes;

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new ProcessJohnsHopkinsCSSE)->everyMinute();
+        $schedule->job(new ProcessJohnsHopkinsCSSE)->everyTenMinutes();
     }
 
     /**
